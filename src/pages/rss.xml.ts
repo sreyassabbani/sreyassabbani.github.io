0bfe57f76d8cc672f5ee679804fde61e321cf495
Sreyas Sabbani <sreyassabbani@gmail.com>
2026-01-11 16:31:58 -0500

    fix: use ts for `rss.xml.ts`

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,13 +1,14 @@
 import { getCollection } from "astro:content";
 import rss from "@astrojs/rss";
 import { SITE_DESCRIPTION, SITE_TITLE } from "../consts";
+import type { APIRoute } from "astro";
 
-export async function GET(context) {
+export const GET: APIRoute = async ({ site }) => {
     const posts = await getCollection("blog");
     return rss({
         title: SITE_TITLE,
         description: SITE_DESCRIPTION,
-        site: context.site,
+        site: site!,
         items: posts.map((post) => ({
             ...post.data,
             link: `/blog/${post.slug}/`,

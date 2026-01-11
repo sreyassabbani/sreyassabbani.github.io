83e6825a4656a06ae8687aaf680d866cd252871e
Sreyas Sabbani <sreyassabbani@gmail.com>
2026-01-11 16:48:17 -0500

    fmt, fix: don't track `README` or `LICENSE` as a post

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,13 +1,15 @@
 import { defineCollection, z } from "astro:content";
+import { glob } from "astro/loaders";
 
 const blog = defineCollection({
-    // Load Markdown and MDX files in src/content/blog.
-    // Type-check frontmatter using a schema
+    loader: glob({
+        pattern: ["**/*.{md,mdx}", "!README.md", "!LICENSE"],
+        base: "./src/content/blog",
+    }),
     schema: ({ image }) =>
         z.object({
             title: z.string(),
             description: z.string(),
-            // Transform string to Date object
             pubDate: z.coerce.date(),
             updatedDate: z.coerce.date().optional(),
             heroImage: image().optional(),
